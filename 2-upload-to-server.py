33af762b057c8ed9f5591e4c82a5e837441e2bd9
Dr. Aryan Jaswal <109587919+aryanjaswaliit@users.noreply.github.com>
2024-02-28 06:11:22 +0000

    commit

---
+++ 2-upload-to-server.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+
+###read the csv file
+df = pd.read_csv("elms-kc-btech-student-detail-kc-btech.csv")
+
+##lenth data in csv
+nlen= len(df)
+n = 0
+while n < nlen-1: 
+    name = df["Name"][n]
+   
+    
+    roll = int(df["University HPTU Roll No"][n])
+    email = df["Email"][n]
+
+
+    
+
+
+    
+    n = n +1
